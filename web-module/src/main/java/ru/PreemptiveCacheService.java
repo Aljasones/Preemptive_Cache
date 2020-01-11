0194851a9d1746aa4342d2945838af4f9fb491d4
Aleksey <aljasones@yandex.ru>
2020-01-11 23:54:20 +0300

    add web module

---
+++ web-module/src/main/java/ru/PreemptiveCacheService.java
@@ -0,0 +1,42 @@
+package ru;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class PreemptiveCacheService extends HttpServlet {
+    int maxSize = 100;
+    PreemptiveCache cache = new PreemptiveCacheImpl(maxSize);
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.setContentType("application/json");
+        int key = Integer.parseInt(req.getParameter("key"));
+
+        String jsonValue = mapper.writeValueAsString(cache.get(key));
+        resp.getWriter().write(jsonValue);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//        resp.setContentType("application/json");
+        String value = req.getParameter("value");
+        int key = Integer.parseInt(req.getParameter("key"));
+
+        cache.put(key, value);
+        String jsonValue = mapper.writeValueAsString(cache.get(key));
+        resp.getWriter().write(jsonValue);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        cache.clear();
+    }
+}
